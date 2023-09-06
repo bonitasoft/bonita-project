7dc9d574dbbc7b06bfb4862e2eae8af211afc1d9
Romain Bioteau <romain.bioteau@bonitasoft.com>
2023-09-06 14:54:29 +0200

    feat(improvements): specific app output
    Avoid multiple archives when clean phase is not executed
    Specify base version for usefull common plugin
    
    Use `bindAllSessionUserProperties` instead of
    `bindSessionUserOverrideProperties` that adds more properties in the
    groovy script context

--- tests/src/it/generate-docker-resources/verify.groovy
+++ tests/src/it/generate-docker-resources/verify.groovy
@@ -1,6 +1,6 @@
 // goal #1
-assert new File(basedir, 'target/application-output/Dockerfile').exists(): 'Dockerfile is missing'
-assert new File(basedir, 'target/application-output/.dockerignore').exists(): '.dockerignore is missing'
+assert new File(basedir, 'target/my-project-parent-local-1.0.0-output/Dockerfile').exists(): 'Dockerfile is missing'
+assert new File(basedir, 'target/my-project-parent-local-1.0.0-output/.dockerignore').exists(): '.dockerignore is missing'
 
 // goal #2
 assert new File(basedir, 'target/test-application-output/Dockerfile').exists(): 'Dockerfile is missing'
