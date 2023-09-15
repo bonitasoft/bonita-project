9480bfbe602b55d3a0cec501c4fee14ad40b561e
Romain Bioteau <romain.bioteau@bonitasoft.com>
2023-09-15 14:15:09 +0200

    feat(application): Application dependency support

--- tests/src/it/generate-docker-resources/verify.groovy
+++ tests/src/it/generate-docker-resources/verify.groovy
@@ -1,7 +1,2 @@
 // goal #1
-assert new File(basedir, 'target/my-project-parent-local-1.0.0-output/Dockerfile').exists(): 'Dockerfile is missing'
-assert new File(basedir, 'target/my-project-parent-local-1.0.0-output/.dockerignore').exists(): '.dockerignore is missing'
-
-// goal #2
-assert new File(basedir, 'target/test-application-output/Dockerfile').exists(): 'Dockerfile is missing'
-assert new File(basedir, 'target/test-application-output/.dockerignore').exists(): '.dockerignore is missing'
+assert new File(basedir, 'target/Dockerfile').exists(): 'Dockerfile is missing'
