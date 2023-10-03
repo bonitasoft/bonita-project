e6ca1fd73d8d9fe81b75953de2d59c1cd592ae72
Romain Bioteau <romain.bioteau@bonitasoft.com>
2023-10-03 09:34:13 +0200

    feat(classpath): Build and include webapp classpath (#15)
    Add an assembly to build a webapp level classpath.
    This classpath is the app module classpath without the bonita connector
    and actor filter included (and their transitive dependencies).
    
    Once generated in the target/classpath folder of the app module, the
    jars of this folder are merged into the bonita/WEB-INF/lib folder of the
    webapp.

--- tests/src/it/package-bundle-tomcat/verify.groovy
+++ tests/src/it/package-bundle-tomcat/verify.groovy
@@ -2,3 +2,10 @@
 def file = new File(basedir, 'app/target/my-project-1.0.0-local-bundle.zip');
 assert file.exists(): 'Bundle Tomcat archive is missing'
 assert file.isFile(): 'Bundle Tomcat archive is not a normal file'
+
+// Assert webapp classpath contains declared dependencies
+def classpathFolder = new File(basedir, 'app/target/classpath/')
+assert classpathFolder.isDirectory() : 'classpath folder is missing'
+assert new File(classpathFolder, 'postgresql-42.6.0.jar').exists() : 'Postgresql driver dependency should be present in the webapp classpath'
+assert new File(classpathFolder, 'checker-qual-3.31.0.jar').exists() : 'Postgresql driver transitive dependency should be present in the webapp classpath'
+assert !new File(classpathFolder, 'bonita-connector-email-1.3.0.jar').exists() : 'bonita-connector-email should not be included in the webapp classpath'
