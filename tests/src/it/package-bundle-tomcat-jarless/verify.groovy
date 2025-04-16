24810d680a5a00cfcf4aefc399ed4946ab49d3de
Vincent Hémery <vincent.hemery@bonitasoft.com>
2025-04-16 12:04:04 +0200

    feat(bar): Possibility to build a jarless BAR
    Test both bar build cases: with and without jar dependencies.
    Restore the script with the guarding condition.
    
    Relates to [BPM-406](https://bonitasoft.atlassian.net/browse/BPM-406)

--- tests/src/it/package-bundle-tomcat-jarless/verify.groovy
+++ tests/src/it/package-bundle-tomcat-jarless/verify.groovy
@@ -8,6 +8,6 @@ def classpathFolder = new File(basedir, 'app/target/classpath/')
 assert classpathFolder.isDirectory() : 'classpath folder is missing'
 assert new File(classpathFolder, 'postgresql-42.7.2.jar').exists() : 'Postgresql driver dependency should be present in the webapp classpath'
 assert new File(classpathFolder, 'checker-qual-3.42.0.jar').exists() : 'Postgresql driver transitive dependency should be present in the webapp classpath'
-assert !new File(classpathFolder, 'bonita-connector-email-1.3.0.jar').exists() : 'bonita-connector-email should not be included in the webapp classpath when bonita.includeDependencyJars=true'
+assert new File(classpathFolder, 'bonita-connector-email-1.3.0.jar').exists() : 'bonita-connector-email should not included in the webapp classpath when bonita.includeDependencyJars=false'
 assert new File(classpathFolder, 'bonita-util-common-4.1.1.jar').exists() : 'Bonita util common dependency should be included in the webapp classpath'
 assert !new File(classpathFolder, 'asm-3.1.jar').exists() : 'Asm transitive dependency should not should be included in the webapp classpath, because it is excluded in the pom.xml'
